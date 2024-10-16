5ab22643460bc386d31da2d5457351343dd3571d
JAMES-RY <jamesrymerbagasbasocero@gmail.com>
2024-10-16 21:25:21 +0800

    QUEUE

---
+++ topic_4_queue/Customer.java
@@ -0,0 +1,20 @@
+
+
+package topic_4_queue;
+
+
+
+public class Customer {
+    private String name;
+
+    public Customer(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+    
+
